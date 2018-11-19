fa4c634d5940eb37820bbb1044ed8a38c7966383
Gary Williams <acadsear@yahoo.com>
2018-11-19 12:54:52 +0800

    Update tabs_below_content.js

--- chrome/userChrome/tabs_below_content.js
+++ chrome/userChrome/tabs_below_content.js
@@ -7,5 +7,25 @@ console.log("tabs_below_content.js");
     
     below_tabs.parentNode.appendChild(tabsbar, below_tabs);
 
-
+    const nav_tb = document.getElementById("navigator-toolbox");
+    var tabsbar_fullscr_observer = new MutationObserver(function(){
+        if(nav_tb.getAttribute("inFullscreen")) // fullscreen
+        {
+            if(document.fullscreen) // video fullscreen
+            {   
+                tabsbar.style.display = "none"; // hide
+            }
+            else // manually browser fullscreen
+            {
+                tabsbar.classList.add("Tabsbar_clps");
+            }
+        }
+        else // not fullscreen
+        {
+            tabsbar.style.display = "";   // unhide
+            tabsbar.classList.remove("Tabsbar_clps");
+        }
+        
+    });
+    tabsbar_fullscr_observer.observe(nav_tb,{attributes:true});
 })();
